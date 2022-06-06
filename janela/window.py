dfc73f6864e402b884c2c24ecc835c7de60067e6
JoaumGabriel <joao.gbr190@gmail.com>
2022-06-06 12:03:29 -0300

    Janela Inicial

--- janela/window.py
+++ janela/window.py
@@ -1,4 +1,8 @@
 from tkinter import *
+from tkinter import ttk
+from tkinter.ttk import Style
+from janela import helper
+
 from game import jogo
 
 class window:
@@ -7,6 +11,7 @@ class window:
     janela.title("Snake Game - Inicio")
     janela.geometry("640x480")
     janela.resizable(width=0, height=0)
+    janela.config(bg="gray")
 
 class buttons:
 
@@ -19,8 +24,28 @@ class buttons:
         window.janela.destroy()
         jogo.play.iniciar()
 
-    startButton = Button(window.janela, text="Começar o jogo", command=abrirJogo)
+    def openHelper():
+
+        window.janela.destroy()
+        helper.windowHelp.openTutorial()
+
+    style = Style()
+
+    style.configure('GameTitle.TLabel', font=('arial', 18, 'bold'), background='gray')
+
+    style.configure('EnterGame.TButton', font=('arial', 11, 'bold'), background='gray')
+    style.configure('Tutorial.TButton', font=('arial', 11, 'bold'), background='gray')
+    style.configure('Exit.TButton', font=('arial', 11, 'bold'), background='gray')
+
+    gameTitle = ttk.Label(window.janela, text="SnakeGame - v1", style='GameTitle.TLabel')
+    gameTitle.place(x=230, y=50)
+
+    startButton = ttk.Button(window.janela, text="Começar o jogo", command=abrirJogo, style='EnterGame.TButton')
     startButton.place(x=250, y=200)
 
-    exitButton = Button(window.janela, text="Sair do jogo", command=fecharJogo)
-    exitButton.place(x=270, y=250)
+    helpButton = ttk.Button(window.janela, text="Como jogar", command=openHelper, style='Tutorial.TButton')
+    helpButton.place(x=260, y=250)
+
+    exitButton = ttk.Button(window.janela, text="Sair do jogo", command=fecharJogo, style='Exit.TButton')
+    exitButton.place(x=260, y=300)
+
