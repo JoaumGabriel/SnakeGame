6542a7925cc8f728b8ca6b88075a57f6c793b6c0
JoaumGabriel <joao.gbr190@gmail.com>
2022-06-06 12:02:01 -0300

    Janela de Tutorial

---
+++ janela/helper.py
@@ -0,0 +1,10 @@
+from tkinter import *
+
+class windowHelp:
+
+    def openTutorial():
+
+        tuto = Tk()
+        tuto.title("Snake Game - Como Jogar")
+        tuto.geometry("640x480")
+        tuto.resizable(width=0, height=0)
